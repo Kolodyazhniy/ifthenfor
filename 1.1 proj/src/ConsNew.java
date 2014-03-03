3cb52c668ef790d230ab205fb33ec7536d4394e8
alexwan <alexwan1122@gmail.com>
2014-03-03 11:38:20 +0200

    Incomplete - task : jva2 nxt lss

---
+++ 1.1 proj/src/ConsNew.java
@@ -0,0 +1,41 @@
+/*
+  Написать приложение, выводящее n строк с переходом и без
+перехода на новую строку.
+   */
+
+public class ConsNew {
+	public static void main(String[] args)
+	{
+	
+		
+		 String var1;
+		 String var2;
+		 String var3; 
+		
+		
+		    var1 = "NoName";
+		    var2 = "jack";
+		    var3 = "xxxx";
+		
+		
+		    	System.out.println("In one line " + var1 + var2  + var3);
+		
+		    
+		    
+			      System.out.println("In new line");
+		    		
+			      System.out.println("" + var1);
+			      System.out.println("" + var2);
+			      System.out.println("" + var3);
+		
+		    		}
+		
+		
+		
+		
+		
+		    
+		    
+	}	
+	
+
