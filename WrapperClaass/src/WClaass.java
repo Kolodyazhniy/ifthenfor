d43b0e9920b90a75bf7cdc04a734f533dc7ad0c2
alexwan <alexwan1122@gmail.com>
2014-03-04 16:38:50 +0200

    Incomplete - task : jva2 nxt lss

--- WrapperClaass/src/WClaass.java
+++ WrapperClaass/src/WClaass.java
@@ -1,7 +1,17 @@
 
 public class WClaass {
+  public static void main (String[] args){	
+	int i = 128;//заменить на 127 !!! 
+	Integer oa = i; //создание объекта+упаковка 
+	Integer ob = i;
+	System.out.println ("oa==i " + (oa == i));// true
+	System.out.println("ob==i " + (ob == i));// true
+	System.out.println("oa==ob " + (oa == ob));// false(ссылки разные)
+	System.out.println("equals ->" + oa.equals(i) 
+			+ ob.equals(i) 
+			+ oa.equals(ob));// true
 	
 	
-	
+}
 
 }
