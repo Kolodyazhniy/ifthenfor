6a28bb390d1c06193d5e045119df858d2a9115bc
alexwan <alexwan1122@gmail.com>
2014-03-06 01:34:38 +0200

    wr

---
+++ wrapped classes/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+
+	
+	public static void main(String[] args) { Integer j = 71; //создание объекта+упаковка 
+	Integer k = ++j; //распаковка+операция+упаковка
+	int i = 2;
+	k = i + j + k;
+
+}
+}
