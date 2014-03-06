27f0175a88e0eaefec9bf0268fe039bf290669a0
alexwan <alexwan1122@gmail.com>
2014-03-06 16:00:34 +0200

    if the for while

--- wrapped classes/src/Main.java
+++ wrapped classes/src/Main.java
@@ -1,12 +1,79 @@
 
 public class Main {
+	public static void main(String[] args) { 
+		
+		int num =199;  /* working if
+		 
+		 */
+		if (num < 100) System.out.println ("num lover then 100");
+		
+		else {
+			System.out.println("num higher the 100");
+		}
+     
+		int sob;  /* working for 
+		
+		*/
+		
+	for(sob=0; // first status
+		sob<10; // condition
+		sob +=1	) // growth	
+		System.out.println ("value of sob" +sob);  // operator
+		
+		/*
+		 * 
+		 * while
+		 * 
+		 * */
+		
+	int money=100;
+	/*
+	while (money>0) // condition (if folce body of cycle dont call)
+		{
+		System.out.println("loss of money" +money);
+		money--;
+		
+		
+	}
+	*/
+	int i, j;
+	i = 100;
+	j = 200;
+	// вычисление среднего значения i и j
+	while(++i < --j) ; // в этом цикле тело цикла отсутствует
+	System.out.println("Среднее значение равно " + i);
 	
-
 	
-	public static void main(String[] args) { Integer j = 71; //создание объекта+упаковка 
-	Integer k = ++j; //распаковка+операция+упаковка
-	int i = 2;
-	k = i + j + k;
-
+	/*
+	 * 
+	 * 
+	 * do while
+	 * 
+	 * 
+	 * */
+	
+	
+	
+	do {
+		
+		System.out.println("loss of dolars " +money);
+		money--;	}
+	while (money>0);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
 }
 }
